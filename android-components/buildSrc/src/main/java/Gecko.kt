1246d6b339927cbd3d7f317441db5086222b18da
Gabriel Luong <gabriel.luong@gmail.com>
2022-10-06 02:34:32 +0000

    Update GeckoView (Nightly) to 107.0.20221005094233.

--- android-components/buildSrc/src/main/java/Gecko.kt
+++ android-components/buildSrc/src/main/java/Gecko.kt
@@ -9,7 +9,7 @@ object Gecko {
     /**
      * GeckoView Version.
      */
-    const val version = "106.0.20220915094451"
+    const val version = "107.0.20221005094233"
 
     /**
      * GeckoView channel
